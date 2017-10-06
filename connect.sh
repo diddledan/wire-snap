9bd6f783a49853b8d3d484e19d054d75a8191aa8
Martin Wimpress <code@flexion.org>
2017-10-06 16:05:05 +0100

    Add connection script.

---
+++ connect.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [ $(id -u) -ne 0 ]; then
+    echo "Root context required please."
+    exit 1
+fi
+
+for INTERFACE in \
+    alsa \
+    bluez \
+    camera \
+    mount-observe; do
+
+    snap connect wire:${INTERFACE} core:${INTERFACE}
+done
+
+
